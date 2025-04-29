e72172f1793d4eb1d498020bff765bf3661c65b3
sandeepa <sanvidurath98@gmail.com>
2025-04-29 10:14:37 +0530

    finished entity in service

--- employee-service/src/main/java/edu/icet/entity/EmployeeEntity.java
+++ employee-service/src/main/java/edu/icet/entity/EmployeeEntity.java
@@ -1,4 +1,40 @@
 package edu.icet.entity;
 
+import edu.icet.util.DepartmentType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employee")
 public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DepartmentType department;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
 }
